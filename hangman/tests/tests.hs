da5aef4149b1839d79508db1fff9519c491f3adc
Daniel J Harvey <dan.harvey@futurice.com>
2018-03-27 13:47:48 +0100

    Make tests somewhat work

---
+++ hangman/tests/tests.hs
@@ -0,0 +1,8 @@
+import Test.Hspec
+import Hangman
+
+main :: IO ()
+main = hspec $ do
+    describe "Addition" $ do
+        it "1 + 1 is greater than 1" $ do
+            (1 + 1) > 1 `shouldBe` True
